cbc1d0f423898f35c04f5a05d1a533fd2b86d437
Joe Dodd <joehdodd@gmail.com>
2021-02-19 09:25:41 -0500

    Fix TS error with filter.ts

--- src/lib/filter.ts
+++ src/lib/filter.ts
@@ -1,24 +1,23 @@
 function strOp(str: string) {
-    return str
-      .toString()
-      .replace(/\s/g, '')
-      .toLowerCase();
-  }
-  
-  function objectValues(value: object) {
-    return Object.values(value).reduce((string, val) => {
-      const test = val !== null && val !== undefined;
-      return (
-        string +
-        (typeof val === 'object' && val !== null
-          ? strOp(objectValues(val))
-          : test ? strOp(val) : '')
-      );
-    }, '');
-  }
-  
-  export function filter(val: string, data: Array<any>) {
-    return data.filter(el => {
-      return !!val.length ? objectValues(el).includes(strOp(val)) : true;
-    });
-  }
+  return str.toString().replace(/\s/g, "").toLowerCase();
+}
+
+function objectValues(value: object): string {
+  return Object.values(value).reduce((string, val) => {
+    const test = val !== null && val !== undefined;
+    return (
+      string +
+      (typeof val === "object" && val !== null
+        ? strOp(objectValues(val))
+        : test
+        ? strOp(val)
+        : "")
+    );
+  }, "");
+}
+
+export function filter(val: string, data: Array<any>) {
+  return data.filter((el) => {
+    return !!val.length ? objectValues(el).includes(strOp(val)) : true;
+  });
+}
